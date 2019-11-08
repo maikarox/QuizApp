ae2668f29ad4c71773230b9f491a5a80eaf21429
maikarox <57074197+maikarox@users.noreply.github.com>
2019-11-08 14:41:06 +0000

    updated test.js

--- backend/test/test.js
+++ backend/test/test.js
@@ -2,11 +2,13 @@ const User = require('../models/User');
 const assert = require('assert');
 let userTest = new User({
     name: '',
-    password: '2hhjer',
+    password: '2hh',
     email: 'emailes.com'
 });
 
-const error = userTest.validateSync();
-assert.equal(error.errors['name'].message, 'User name is required');
-assert.equal(error.errors['password'].message, 'Password must have 6 characters or more');
-assert.equal(error.errors['email'].message, 'It is not a valid email');
+userTest.validate().catch(error => {
+    assert.ok(error);
+    assert.equal(error.errors['name'].message, 'User name is required');
+    assert.equal(error.errors['password'].message, 'Password must have 6 characters or more');
+    assert.equal(error.errors['email'].message, 'It is not a valid email');
+});
